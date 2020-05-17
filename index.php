1aaff2d8e608f14caa6e51b377bcd246e3d0203e
Alexnellfors <alex.nellfors@gmail.com>
2020-05-17 12:56:09 +0200

    ändrat bakgrund

--- index.php
+++ index.php
@@ -1,13 +1,16 @@
 <html>
 <head>
 DiscooWeather Homepage
+<link rel="stylesheet" type="text/css" href="discoCSS.css"/>
 </head>
 <body>
+
 <test> hejhej </test>
 <header>
     <h1>DiscooWeather</h1>
     <h3>Där solen alltid skiner!</h3>
 </header>
     WORKING 9 2 5!
+    nu ändrat till html
 </body>
 </html>
