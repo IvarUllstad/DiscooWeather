ecec7845f8fc59938268761c14c62e2878cf7534
TomHalvarsson <Tom.Halvarsson@hotmail.com>
2020-05-20 15:37:31 +0200

    väder

--- Registration.php
+++ Registration.php
@@ -2,30 +2,20 @@
 
 $username = $_POST['username'];
 $email = $_POST['email'];
-$password = $_POST['password'];
-Registrera($username, $email, $password);
+$lösenord = $_POST['lösenord'];
+Registrera($username, $email, $lösenord);
 
-function Registrera($username, $email, $password)
+function Registrera($username, $email, $lösenord)
 {
     $db=new SQLite3("Users.db");
 
     $sql="INSERT INTO UserList (Username, Email, Lösenord)
-    VALUES (:username, :email, :password)";
+    VALUES (:username, :email, :lösenord)";
     $stmt = $db->prepare($sql);
     $stmt->bindParam(':username', $username, SQLITE3_TEXT);
     $stmt->bindParam(':email', $mail, SQLITE3_TEXT);
-    $stmt->bindParam(':password', $password, SQLITE3_TEXT);
-    if($stmt -> execute())
-    {
-        
-        $db -> close();
-        return true;
-    }
-    else
-    {
-        $db -> close();
-        return false ;              
-    }
+    $stmt->bindParam(':lösenord', $lösenord, SQLITE3_TEXT);
+    
 
 }
 
---
+++ Weather.php
@@ -0,0 +1,43 @@
+<!DOCTYPE HTML>
+
+<?php
+
+  $city = $_POST['stad'];
+$url = "http://api.openweathermap.org/data/2.5/find?q=$city&units=metric&type=accurate&mode=xml&APPID=1407925d5eb998aa25042dffa58983c5";
+$getweather = simplexml_load_file($url);
+$gethumidity = $getweather->list->item->humidity['value'];
+$gettemp = $getweather->list->item->temperature['value'];
+$getspeed = $getweather->list->item->wind->speed['value'];
+$getcloud = $getweather->list->item->weather['value'];
+$getprecipitation = $getweather->list->item->precipitation['mode'];
+$getlastupdate = $getweather->list->item->lastupdate['value'];
+
+
+?>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>City weather</title>
+        <link rel="stylesheet" type="text/css" href="discoCSS.css"/>
+    </head>
+    <body>
+        <h1>Välj en stad!</h1>
+        <form method="POST">
+        <input type="text" name="stad" placeholder="Välj stad">
+        <button type="submit">Submit</button><br>
+        </form>
+        <div class="top10table">
+        <ul>
+            <?php echo "<p style='color:green; font-size:30px;'>", $city, "</p>"; ?>
+            <?php echo "<li style='color:red; font-size:20px;'>", 'Luftuktigheten är: ', ($gethumidity), "</li>"; ?>
+            <?php echo "<li style='color:red; font-size:20px;'>", 'Temperaturen är: ', ($gettemp), ' grader celsius', "</li>"; ?>
+            <?php echo "<li style='color:red; font-size:20px;'>", 'Vindhastigheten är: ', ($getspeed), ' m/s', "</li>"; ?>
+            <?php echo "<li style='color:red; font-size:20px;'>", 'Vädret är: ', ($getcloud), "</li>"; ?>
+            <?php echo "<li style='color:red; font-size:20px;'>", 'Nederbörd: ', ($getprecipitation), "</li>"; ?>
+            <?php echo "<li style='color:red; font-size:20px;'>", 'Senaste uppdateringen: ', ($getlastupdate), "</li>"; ?>
+            
+        </ul>
+        </div>
+    </body>
+
+</html>
