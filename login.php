611fabe6385124d21dfa35d5abc5e786a708b135
IvarUllstad <ivull666@gmail.com>
2020-05-30 11:40:56 +0200

    stjärnor på lösenorden

--- login.php
+++ login.php
@@ -9,7 +9,7 @@
                 <label for="username"> Username:</label><br>
                 <input type="text" id="username" name="username"><br>
                 <label for="password"> Password:</label><br>
-                <input type="text" id="password" name="password"><br>
+                <input type="password" id="password" name="password"><br>
             </fieldset>
             <input type="submit" value="Submit">
         </form>
