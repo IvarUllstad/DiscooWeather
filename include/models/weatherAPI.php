2b33490063396aae439564fb0a16ca4f9e3299e5
IvarUllstad <ivull666@gmail.com>
2020-05-30 11:58:30 +0200

    funktion av weatherAPI

--- include/models/weatherAPI.php
+++ include/models/weatherAPI.php
@@ -1,4 +1,11 @@
 <?php
+
+    echo'<h3>Välj en stad!</h3>
+        <form method="POST">
+            <input type="text" name="stad" placeholder="Välj stad">
+            <button type="submit">Submit</button><br>
+        </form>';
+    function getWeather(){
     $city = $_POST['stad'];
     $url = "http://api.openweathermap.org/data/2.5/find?q=$city&units=metric&type=accurate&mode=xml&APPID=1407925d5eb998aa25042dffa58983c5";
     $getweather = simplexml_load_file($url);
@@ -9,17 +16,12 @@
     $getprecipitation = $getweather->list->item->precipitation['mode'];
     $getlastupdate = $getweather->list->item->lastupdate['value'];
 
-    echo'<h3>Välj en stad!</h3>
-        <form method="POST">
-            <input type="text" name="stad" placeholder="Välj stad">
-            <button type="submit">Submit</button><br>
-        </form>';
-
     echo "<p style='color:green; font-size:30px;'>", $city, "</p>";
     echo "<li style='color:red; font-size:20px;'>", 'Luftuktigheten är: ', ($gethumidity), "</li>";
     echo "<li style='color:red; font-size:20px;'>", 'Temperaturen är: ', ($gettemp), ' grader celsius', "</li>";
     echo "<li style='color:red; font-size:20px;'>", 'Vindhastigheten är: ', ($getspeed), ' m/s', "</li>";
     echo "<li style='color:red; font-size:20px;'>", 'Vädret är: ', ($getcloud), "</li>";
     echo "<li style='color:red; font-size:20px;'>", 'Nederbörd: ', ($getprecipitation), "</li>";
-    echo "<li style='color:red; font-size:20px;'>", 'Senaste uppdateringen: ', ($getlastupdate), "</li>";        
+    echo "<li style='color:red; font-size:20px;'>", 'Senaste uppdateringen: ', ($getlastupdate), "</li>"; 
+    }       
 ?>
